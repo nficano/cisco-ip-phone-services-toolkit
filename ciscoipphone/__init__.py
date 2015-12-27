a763f9b6d861f380f50e5894fca9cc2adabad848
Nick Ficano <nficano@gmail.com>
2015-12-27 15:57:16 -0500

    Release version 0.1.0

--- ciscoipphone/__init__.py
+++ ciscoipphone/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 __title__ = 'ciscoipphone'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2015 Nick Ficano'
