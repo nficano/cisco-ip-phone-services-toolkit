babf0f5c9213bd464cdee727ad5bd2f6c0135227
Nick Ficano <nficano@gmail.com>
2018-07-01 19:04:00 -0400

    Release: 2.0.13.dev0 → 2.0.14.dev0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 
 setup(
     name='cisco_ip_phone_services',
-    version='2.0.13.dev0',
+    version='2.0.14.dev0',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['cisco_ip_phone_services'],
