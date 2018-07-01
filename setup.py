4bda6f8d8a436e77ea8b137af2483a788da99f85
Nick Ficano <nficano@gmail.com>
2018-07-01 14:19:08 -0400

    Release: 2.0.4.dev0 → 2.0.5.dev0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 
 setup(
     name='phone_services',
-    version='2.0.4.dev0',
+    version='2.0.5.dev0',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['phone_services'],
