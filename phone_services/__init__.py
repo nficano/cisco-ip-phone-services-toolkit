389b4684a950bf05828a8c5831feb3bf597d66a0
Nick Ficano <nficano@gmail.com>
2018-07-01 14:11:30 -0400

    Release: 2.0.2.dev0 → 2.0.2

--- phone_services/__init__.py
+++ phone_services/__init__.py
@@ -4,7 +4,7 @@
 Phone Services: A Factory for building Cisco IP Phone Services.
 """
 __title__ = 'phone_services'
-__version__ = '2.0.2.dev0'
+__version__ = '2.0.2'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2018 Nick Ficano'
